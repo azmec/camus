6cd412d53709039b5d9b03c8b2bf0e71464e0cd5
aldats <aldats@aldats.dev>
2022-01-21 16:36:04 -0600

    Update comments for `ComponentArray`
    Bringing comments up to standard and properly aligning their syntax with
    `LDoc`.

--- camus/component-array.lua
+++ camus/component-array.lua
@@ -1,12 +1,12 @@
---- SUMMARY
+--- Sequential table containing entity component data.
 -- @classmod ComponentArray
 
 local ComponentArray = {}
 ComponentArray.__mt = { __index = ComponentArray }
 
 --- Constructs a new ComponentArray.
--- @param constructor function
--- @return ComponentArray
+-- @tparam function constructor A function which returns component data. 
+-- @treturn ComponentArray
 ComponentArray.new = function(constructor)
     return setmetatable({
         constructor = constructor,
@@ -15,28 +15,28 @@ ComponentArray.new = function(constructor)
 end
 
 --- Constructs a new component for the given entity.
--- @param entity number
--- @param ... Arguments unique to the constructor.
+-- @tparam number entity An entity's ID.
+-- @tparam {...} ... Arguments unique to the constructor.
 ComponentArray.construct = function(self, entity, ...)
     self.entities[entity] = self.constructor(...)
 end
 
 --- Returns a reference to the given entity's component data.
--- @param entity number
--- @return Variant
+-- @tparam number entity An entity's ID.
+-- @treturn {...} Data unique to the component.
 ComponentArray.peak = function(self, entity)
     return self.entities[entity]
 end
 
 --- Destroys the component data of the given entity.
--- @param entity number
+-- @tparam number entity An entity's ID.
 ComponentArray.destroy = function(self, entity)
     self.entities = nil
 end
     
 --- Returns true/false if the given entity has component data.
--- @param entity number
--- @return bool
+-- @tparam entity number An entity's ID.
+-- @treturn bool If the entity has this ComponentArray's component data.
 ComponentArray.has = function(self, entity)
     return self.entities[entity] ~= nil
 end
