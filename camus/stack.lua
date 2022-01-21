989679d81d4a29699ac31bfcf247cddbcbeabed7
aldats <aldats@aldats.dev>
2022-01-21 17:05:30 -0600

    Update comments for `Stack`

--- camus/stack.lua
+++ camus/stack.lua
@@ -1,24 +1,30 @@
 --- Simple Last In First Out list implementation.
--- I already know; I like sugar.
 -- @classmod Stack
 
 local Stack = {}
 Stack.__mt = { __index = Stack }
 
+--- Construct a new Stack.
+-- @treturn Stack
 Stack.new = function()
     return setmetatable({}, Stack.__mt)
 end
 
+--- Push an element onto the Stack.
+-- @tparam ... i Element of any type.
 Stack.push = function(self, i)
     self[#self + 1] = i
 end
 
-Stack.pop = function(self, i)
+--- Pop the most recently pushed element off of the Stack.
+Stack.pop = function(self)
     e = self[#self]
     self[#self] = nil
     return e
 end
 
+--- Return the count of elements in the Stack.
+-- @treturn number Count of elements in the Stack.
 Stack.size = function(self)
     return #self
 end
