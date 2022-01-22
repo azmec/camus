68aba25a43c27842c8a7315b6dc547bcd8b36666
aldats <aldats@aldats.dev>
2022-01-21 20:31:17 -0600

    Edit comments for `ComponentArray`
    Move verbage from "This function..." to "This function will..."

--- camus/component-array.lua
+++ camus/component-array.lua
@@ -4,7 +4,7 @@
 local ComponentArray = {}
 ComponentArray.__mt = { __index = ComponentArray }
 
---- Constructs a new ComponentArray.
+--- Construct a new ComponentArray.
 -- @tparam func constructor A function which returns component data. 
 -- @treturn ComponentArray
 ComponentArray.new = function(constructor)
@@ -14,27 +14,27 @@ ComponentArray.new = function(constructor)
     }, ComponentArray.__mt)
 end
 
---- Constructs a new component for the given entity.
+--- Construct a new component for the entity.
 -- @tparam int entity An entity's ID.
 -- @tparam {...} ... Arguments unique to the constructor.
 ComponentArray.construct = function(self, entity, ...)
     self.entities[entity] = self.constructor(...)
 end
 
---- Returns a reference to the given entity's component data.
+--- Return the entity's component data.
 -- @tparam int entity An entity's ID.
 -- @treturn {...} Data unique to the component.
 ComponentArray.peak = function(self, entity)
     return self.entities[entity]
 end
 
---- Destroys the component data of the given entity.
+--- Destroy the component data of the given entity.
 -- @tparam int entity An entity's ID.
 ComponentArray.destroy = function(self, entity)
     self.entities = nil
 end
     
---- Returns true/false if the given entity has component data.
+--- Returns if the entity has component data in this ComponentArray.
 -- @tparam int entity An entity's ID.
 -- @treturn bool If the entity has this ComponentArray's component data.
 ComponentArray.has = function(self, entity)
